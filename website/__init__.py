83a7a3cf37625c8c568a199f2f5aa3abc8724e2e
Dodica <occupied.fall@gmail.com>
2023-11-06 02:21:46 +0100

    add broken authentication vulnerability toggle

--- website/__init__.py
+++ website/__init__.py
@@ -2,13 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from flask_caching import Cache
 
+cache = Cache(config={'CACHE_TYPE': 'simple'})
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
+    cache.init_app(app)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
--- website/auth.py
+++ website/auth.py
@@ -1,29 +1,75 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, current_app
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db   ##means from __init__.py import db
+from . import db
 from flask_login import login_user, login_required, logout_user, current_user
-
+from . import cache
 
 auth = Blueprint('auth', __name__)
+MAX_LOGIN_ATTEMPTS = 5
+RATE_LIMIT_DURATION = 3600
+RATE_LIMIT_RESET_KEY = 'rate_limit_reset_{}'
 
+def is_rate_limited(ip_address, cache):
+    """
+    Check if the IP address is rate-limited.
+    """
+    rate_limit_key = RATE_LIMIT_RESET_KEY.format(ip_address)
+    count = cache.get(rate_limit_key)
+    return count is not None and count >= MAX_LOGIN_ATTEMPTS
+
+def reset_rate_limit(ip_address, cache):
+    """
+    Reset the rate limit for an IP address.
+    """
+    rate_limit_key = RATE_LIMIT_RESET_KEY.format(ip_address)
+    cache.delete(rate_limit_key)
+
+def increment_login_attempts(ip_address, cache):
+    """
+    Increment the login attempt count for an IP address.
+    """
+    rate_limit_key = RATE_LIMIT_RESET_KEY.format(ip_address)
+    count = cache.get(rate_limit_key)
+    if count is None:
+        count = 0
+    count += 1
+    cache.set(rate_limit_key, count, timeout=RATE_LIMIT_DURATION)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-
+        show_password_rules = 'showPasswordRules' in request.form
         user = User.query.filter_by(email=email).first()
+        print(show_password_rules)
+        if is_rate_limited(request.remote_addr, cache):
+            flash('Too many login attempts. Please try again later.', category='error')
+            return redirect(url_for('auth.login'))
+
         if user:
-            if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
-                login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+            if not show_password_rules:
+                if check_password_hash(user.password, password):
+                    flash('Logged in successfully!', category='success')
+                    login_user(user, remember=True)
+                    reset_rate_limit(request.remote_addr, cache)
+                    return redirect(url_for('views.home'))
+                else:
+                    flash('Incorrect password, try again.', category='error')
             else:
-                flash('Incorrect password, try again.', category='error')
+                if user.password == password:
+                    flash('Logged in successfully!', category='success')
+                    login_user(user, remember=True)
+                    reset_rate_limit(request.remote_addr, cache)
+                    return redirect(url_for('views.home'))
+                else:
+                    flash('Incorrect password, try again.', category='error')
+
         else:
             flash('Email does not exist.', category='error')
+
+        increment_login_attempts(request.remote_addr, cache)
 
     return render_template("login.html", user=current_user)
 
@@ -42,21 +88,30 @@ def sign_up():
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+        show_password_rules = 'showPasswordRules' in request.form
 
         user = User.query.filter_by(email=email).first()
-        if user:
-            flash('Email already exists.', category='error')
-        elif len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
-        elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
-        elif password1 != password2:
-            flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
+        if not show_password_rules:
+            if user:
+                flash('Email already exists.', category='error')
+            elif len(email) < 4:
+                flash('Email must be greater than 3 characters.', category='error')
+            elif len(first_name) < 2:
+                flash('First name must be greater than 1 character.', category='error')
+            elif password1 != password2:
+                flash('Passwords don\'t match.', category='error')
+            elif len(password1) < 7:
+                flash('Password must be at least 7 characters.', category='error')
+            else:
+                new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                    password1, method='pbkdf2:sha256'))
+                db.session.add(new_user)
+                db.session.commit()
+                login_user(new_user, remember=True)
+                flash('Account created!', category='success')
+                return redirect(url_for('views.home'))
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name, password=password1)
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
